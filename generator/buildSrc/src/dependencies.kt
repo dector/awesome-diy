9dcf1b859c1b2ccdcec1805b3a7eea9fba6232ac
Denys M <dector9@gmail.com>
2020-06-07 11:50:19 +0400

    Count items in raw data

--- generator/buildSrc/src/dependencies.kt
+++ generator/buildSrc/src/dependencies.kt
@@ -1,10 +1,14 @@
 object Deps {
     const val kotlin_coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.kotlin_coroutines}"
     const val kotlin_test = "io.kotlintest:kotlintest-runner-junit5:${Versions.kotlin_test}"
+
+    const val hjson = "org.hjson:hjson:${Versions.hjson}"
 }
 
 object Versions {
     const val kotlin = "1.3.60"
     const val kotlin_coroutines = "1.3.2"
     const val kotlin_test = "3.4.2"
+
+    const val hjson = "3.0.0"
 }
--- generator/src/main/kotlin/io/github/dector/project/main.kt
+++ generator/src/main/kotlin/io/github/dector/project/main.kt
@@ -1,5 +1,15 @@
 package io.github.dector.project
 
+import org.hjson.JsonValue
+import java.io.File
+
 fun main() {
-    println("It works!")
+    val file = File("data/collection.hjson")
+
+    println("Reading from ${file.absolutePath}")
+
+    val data = JsonValue.readHjson(file.reader()).asObject()
+    val items = data["items"].asArray()
+
+    println("Found ${items.size()} items")
 }
